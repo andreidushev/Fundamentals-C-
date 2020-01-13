7ac50de5061b6c6fa084cf1b3361fdbd01a9cc64
Andrei Dushev <andrei4o6@abv.bg>
2020-01-13 15:04:55 +0200

    Back in 30 mins

---
+++ Fundamentals C/Back in 30 mins/Back in 30 mins.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Back_in_30_mins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            minutes += 30;
+
+            if (minutes > 59)
+            {
+                hours++;
+                minutes -= 60;
+            }
+            if (hours > 23)
+            {
+                hours -= 24;
+            }
+            string output = $"{hours}:{minutes:D2}";
+            Console.WriteLine(output);
+        }
+    }
+}
