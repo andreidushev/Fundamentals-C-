abddcfdaafe76e197f2a490796903aae3f11f419
Andrei Dushev <andrei4o6@abv.bg>
2020-01-19 17:49:40 +0200

    Ages

---
+++ Fundamentals C/01 Ages/01 Ages.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _01_Ages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            string person = string.Empty;
+
+            if (input <= 2)
+            {
+                person = "baby";
+            }
+            else if (input <= 13)
+            {
+                person = "child";
+            }
+            else if (input <= 19)
+            {
+                person = "teenager";
+            }
+            else if (input <= 65)
+            {
+                person = "adult";
+            }
+            else
+            {
+                person = "elder";
+            }
+
+            Console.WriteLine(person);
+        }
+    }
+}
