11b4964caf354225b3d4bc6492d5b11922edae87
Andrei Dushev <andrei4o6@abv.bg>
2020-01-19 17:44:51 +0200

    Login

---
+++ Fundamentals C/Login/Login.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Login
+{
+    class Login
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+
+            string correct = string.Empty;
+
+            for (int i = username.Length -1; i >= 0; i--)
+            {
+                correct += username[i];
+            }
+
+            bool blocked = true;
+
+            for (int i = 0; i < 3; i++)
+            {
+                string pass = Console.ReadLine();
+
+                if (correct == pass)
+                {
+                    blocked = false;
+                    Console.WriteLine($"User {username} logged in.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect password. Try again.");
+                }
+            }
+
+            if (blocked)
+            {
+                Console.WriteLine($"User {username} blocked!");
+            }
+        }
+    }
+}
