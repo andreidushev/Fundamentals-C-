b8051d1014243db66104c573c3e9ff9e4955bdab
Andrei Dushev <andrei4o6@abv.bg>
2020-01-19 16:20:00 +0200

    Multiplication Table 2.0

---
+++ Fundamentals C/Multiplication Table 2.0/Multiplication Table 2.0.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Multiplication_Table_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
