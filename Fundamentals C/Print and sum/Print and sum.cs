322784e80307a25618752d388275f7964aece5e8
Andrei Dushev <andrei4o6@abv.bg>
2020-01-19 17:13:13 +0200

    Print and sum

---
+++ Fundamentals C/Print and sum/Print and sum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Print_and_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            int num = a;
+
+            for (int i = a; i <= b; i++)
+            {
+                sum += i;
+                Console.Write($"{num} ");
+                num ++;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
