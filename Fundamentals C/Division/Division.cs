10a3b17f4f01d953cc41bd4f098c042c241612e1
Andrei Dushev <andrei4o6@abv.bg>
2020-01-19 16:19:23 +0200

    Division

---
+++ Fundamentals C/Division/Division.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Division
+{
+    class Division
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int num = 0;
+
+            if (n % 10 == 0)
+            {
+                num = 10;
+            }
+            else if (n % 7 == 0)
+            {
+                num = 7;
+            }
+            else if (n % 6 == 0)
+            {
+                num = 6;
+            }
+            else if (n % 3 == 0)
+            {
+                num = 3;
+            }
+            else if (n % 2 == 0)
+            {
+                num = 2;
+            }
+            if (num != 0)
+            {
+                Console.WriteLine($"The number is divisible by {num}");
+            }
+            else
+            {
+                Console.WriteLine("Not divisible");
+            }
+        }
+    }
+}
